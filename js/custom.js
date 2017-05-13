0ff3e825bf96ceb85d3ec6dc4f0802642aad91b7
Aleksey <u608110@gmai.com>
2017-05-13 14:04:45 +0300

    Accordion

--- js/custom.js
+++ js/custom.js
@@ -40,4 +40,33 @@ $(function() {
         }, 700);
     });
 
+    // Show Modal Window
+    $('.js-show-modal').on('click', function (e) {
+        e.preventDefault();
+        var currentModal = $(this).attr('href');
+        $('body').addClass('open-modal');
+        $(currentModal + ', #js-overlay').fadeIn(500);
+
+       $('.js-modal-close, #js-overlay').on('click', function (e) {
+            e.preventDefault();
+            $('body').removeClass('open-modal');
+            $(currentModal + ', #js-overlay').fadeOut(500);
+        });
+    });
+
+    // FAQ Accordion
+    $(".js-faq-title").on("click", function(e) {
+
+        e.preventDefault();
+        var $this = $(this);
+
+        if( !$this.hasClass("active") ) {
+            $(".js-faq-content").slideUp();
+            $(".js-faq-title").removeClass("active");
+        }
+
+        $this.toggleClass("active");
+        $this.next().slideToggle();
+
+    });
 });
